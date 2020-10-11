0095effaf86f3aab42c814b58a49bb93de76bdef
peter <forLichess123@yandex.ru>
2020-10-11 16:05:51 +0300

    new lesson

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
 const personalMovieDb = {
     count:numberOfFilms,
@@ -7,8 +8,26 @@ const personalMovieDb = {
     privat:false
 };
   for (let i = 0; i < 2; i++){
-const lastViewedFilms = prompt('Один из последних просмотренных фильмов?','');
-const userVotes = prompt('На сколько вы оцените его?','');
-personalMovieDb.movies[lastViewedFilms]=userVotes;
+    const  lastViewedFilms = prompt('Один из последних просмотренных фильмов?',''),
+           userVotes = prompt('На сколько вы оцените его?','');
+        if (lastViewedFilms != null && lastViewedFilms.length != 0 && 
+            userVotes != null && lastViewedFilms.length < 50 && userVotes.length !=0){
+                personalMovieDb.movies[lastViewedFilms]=userVotes;
+                console.log('sucsess');
+            }else{
+                i--;
+                console.log('error');
+            }
+    
+    
+}
+  if (personalMovieDb.count <= 10){
+      console.log('Вы посмотрели мало фильмов');
+  }else if(personalMovieDb.count > 10 && personalMovieDb.count < 30){
+    console.log('Вы классический зритель');
+  }else if(personalMovieDb.count >= 30 ){
+    console.log('Вы киноман!');
+  }else{
+    console.log('Произошла ошибка!');
   }
 console.log(personalMovieDb);
